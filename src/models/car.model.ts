0df169b379b13a5ee35cb96cdf8fd9823855cf2d
GabrielSilper <gabrielsilper.main@gmail.com>
2023-07-29 08:23:27 -0400

    Formatei o JSON de retorno com o mongoose

--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -1,24 +1,35 @@
 import { Schema, model } from 'mongoose';
 import ICar from '../interfaces/ICar';
 
-const CarSchema = new Schema<ICar>({
-  // _id: {
-  //   type: Number,
-  //   required: true,
-  // },
-  name: {
-    type: String,
-    required: true,
+const CarSchema = new Schema<ICar>(
+  {
+    // _id: {
+    //   type: Number,
+    //   required: true,
+    // },
+    name: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
   },
-  brand: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 const CarModel = model<ICar>('Car', CarSchema);
 
