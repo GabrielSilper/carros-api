fb410584903b2963bdce03c81ee076b7116e7e22
GabrielSilper <gabrielsilper.main@gmail.com>
2023-07-29 05:39:52 -0400

    Configuração inicial do Mongoose ok

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 class App {
   public app: express.Express;
+  private db = mongoose; // sei que poderia criar um interface pra acoplar diversos bancos, mas aqui só queria praticar
 
   constructor() {
     this.app = express();
@@ -23,6 +25,15 @@ class App {
 
   public start(PORT: string | number): void {
     this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+
+  public async connectDB(uri: string): Promise<void> {
+    try {
+      await this.db.connect(uri);
+      console.log(`Good connection with database`);
+    } catch (error) {
+      console.log(`You have a trouble with the connection: ${error}`);
+    }
   }
 }
 
