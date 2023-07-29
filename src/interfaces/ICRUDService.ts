a77aad91790a632d889a91852f5dfaa19c7e0d4f
GabrielSilper <gabrielsilper.main@gmail.com>
2023-07-29 06:58:02 -0400

    Melhorei a interface de crud pra receber tipos genéricos e fiz CarService implementar ICRUDService<T>

--- src/interfaces/ICRUDService.ts
+++ src/interfaces/ICRUDService.ts
@@ -2,11 +2,11 @@ import { NewEntity } from '.';
 import ICar from './ICar';
 import ServiceData from './ServiceData';
 
-export default interface ICRUDService {
+export default interface ICRUDService<T> {
   // para adicionar um novo carro no banco.
-  add?(car: NewEntity<ICar>): Promise<ServiceData<ICar>>;
-  findAll?(): Promise<ServiceData<ICar[]>>;
-  findById?(id: number): Promise<ServiceData<ICar>>;
-  update?(id: number): Promise<ServiceData<ICar>>;
+  add(car: NewEntity<T>): Promise<ServiceData<T>>;
+  findAll(): Promise<ServiceData<T[]>>;
+  findById?(id: number): Promise<ServiceData<T>>;
+  update?(id: number): Promise<ServiceData<T>>;
   delete?(): Promise<void>;
 }
--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,1 +1,13 @@
-class CarService {}
+import { NewEntity } from '../interfaces';
+import ICRUDService from '../interfaces/ICRUDService';
+import ICar from '../interfaces/ICar';
+import ServiceData from '../interfaces/ServiceData';
+
+class CarService implements ICRUDService<ICar> {
+  add(car: NewEntity<ICar>): Promise<ServiceData<ICar>> {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): Promise<ServiceData<ICar[]>> {
+    throw new Error('Method not implemented.');
+  }
+}
