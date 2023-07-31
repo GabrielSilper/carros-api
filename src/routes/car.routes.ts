7664713b1ed5c7214fa81a1641df9b4be680dfd6
GabrielSilper <gabrielsilper.main@gmail.com>
2023-07-31 17:51:26 -0400

    O CRUD da API da REST está completo agora

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -8,8 +8,20 @@ carRouter.get('/', (req: Request, res: Response) =>
   carController.findAll(req, res)
 );
 
+carRouter.get('/:id', (req: Request, res: Response) =>
+  carController.findById(req, res)
+);
+
 carRouter.post('/', (req: Request, res: Response) =>
   carController.create(req, res)
+);
+
+carRouter.patch('/:id', (req: Request, res: Response) =>
+  carController.update(req, res)
+);
+
+carRouter.delete('/:id', (req: Request, res: Response) =>
+  carController.delete(req, res)
 );
 
 export default carRouter;
